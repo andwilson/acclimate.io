6cbbd4c340ba533b29bafb426c5fef4269934b45
Andrew Wilson <5093707+andwilson@users.noreply.github.com>
2018-10-15 14:21:01 -0700

    edited config

--- gatsby-config.js
+++ gatsby-config.js
@@ -48,6 +48,13 @@ module.exports = {
         name: "img"
       }
     },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/src/pages`,
+        name: "pages"
+      }
+    },
     `gatsby-transformer-sharp`,
     // 'gatsby-transformer-remark',
     `gatsby-plugin-sharp`,
