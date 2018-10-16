32bc3179e09d2b55a46d943fb946c5aa1ec01717
Andrew Wilson <5093707+andwilson@users.noreply.github.com>
2018-10-16 15:13:44 -0700

    Refactored code to write pure components and remove unused libraries

---
+++ src/pages/blog.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Helmet from "react-helmet";
+
+export default () => (
+  <div>
+    <Helmet>
+      <title>Acclimate | Blog</title>
+    </Helmet>
+    <div>
+        <h1>Blog page</h1>
+    </div>
+  </div>
+);
--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -6,7 +6,7 @@ import { colors } from "../styles/theme";
 import SectionWrapper from "../components/SectionWrapper";
 
 const Title = styled.h1`
-  color: ${colors.primary};
+  color: ${colors.secondary};
   margin: 0;
 `;
 
@@ -19,24 +19,17 @@ const Div = styled.div`
   border-top: 1px solid grey;
 `;
 
-class BlogPostTemplate extends React.Component {
-  render() {
-    const post = this.props.data.markdownRemark;
+export default ({data}) => (
 
-    return (
       <SectionWrapper>
         <div>
-          <Helmet title={`${post.frontmatter.title}`} />
-          <Title>{post.frontmatter.title}</Title>
-          <Date>{post.frontmatter.date}</Date>
-          <Div dangerouslySetInnerHTML={{ __html: post.html }} />
+          <Helmet title={`${data.markdownRemark.frontmatter.title}`} />
+          <Title>{data.markdownRemark.frontmatter.title}</Title>
+          <Date>{data.markdownRemark.frontmatter.date}</Date>
+          <Div dangerouslySetInnerHTML={{ __html: data.markdownRemark.html }} />
         </div>
       </SectionWrapper>
     );
-  }
-}
-
-export default BlogPostTemplate;
 
 export const pageQuery = graphql`
   query BlogPostByPath($path: String!) {
@@ -46,6 +39,7 @@ export const pageQuery = graphql`
       frontmatter {
         title
         date(formatString: "MMMM D, YYYY")
+        author
       }
     }
   }
