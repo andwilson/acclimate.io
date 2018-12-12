970859914305dd1b43f9341332979916af25c39b
Andrew Wilson <5093707+andwilson@users.noreply.github.com>
2018-12-12 12:22:10 -0800

    Edited copy on the careers page

--- src/pages/careers.js
+++ src/pages/careers.js
@@ -33,7 +33,7 @@ export default () => (
           We believe that technology is a pervasive force that creates massive
           opportunities for those who navigate it, and unprecedented threats to
           those who don't. Our vision is to help companies who don't have access
-          to a world-class data science and software engineering team leverage
+          to world-class data science and software engineering teams to leverage
           state-of-the-art machine learning algorithms and modern software
           toolsets to stay competitive and thrive in this new climate.
         </p>
@@ -43,24 +43,25 @@ export default () => (
       <div>
         <h1>Our core values</h1>
         <p>
-          <b>Client value creation</b>: Clients are a the center of all we do
-          and our job is to solve their problems and make them more productive.
-          Look for opportunities to create value through tools, repeatable
-          processes and/or product creation. Share knowledge and experiences
-          with colleagues to increase the overall value we provide as a team.
+          <b>Client value creation</b>: Our core focus is to solve client
+          problems and to make them operate more effectively. Seek out
+          opportunities to delight customers in unique ways. Share knowledge and
+          experiences with colleagues and build internal tools to increase the
+          overall value we provide as a team.
         </p>
         <p>
           <b>Relationships & communication</b>: Build strong, meaningful, and
-          enjoyable relationships in work and life. To succeed, use technology
-          to collaborate and deliberately over-communicate. To fail, use
-          technology to isolate yourself and ignore others. Cultivate a team and
-          culture that you love and find fulfilling.
+          enjoyable relationships in work and in life. To succeed, use
+          technology to collaborate and deliberately over-communicate. To fail,
+          use technology to isolate yourself and ignore others. Cultivate a team
+          and culture that you love and find fulfilling.
         </p>
         <p>
           <b>Continous, applied learning</b>: Technology moves quickly and
           business churn is faster than ever. Always be learning and always
-          apply what you learned. Prioritize learning from people and from doing
-          over texbooks and documentation.
+          apply what you've learned. Priotitize learning from conversations and
+          completing practical projects over learning from texbooks and
+          documentation.
         </p>
       </div>
     </SectionWrapper>
